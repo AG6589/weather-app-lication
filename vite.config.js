c16df49838ae6fe679b5005d77e113b8ec7b7d6d
Aman gupta <117616487+AG6589@users.noreply.github.com>
2026-01-29 17:26:08 +0530

    Fix: switch to relative base and paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weather-app-lication/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
